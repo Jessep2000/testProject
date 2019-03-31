df44e28b40660ab7932aa383d6f993438ae1fbc2
Jessep2000 <42765970+Jessep2000@users.noreply.github.com>
2019-03-31 19:06:45 +0200

    added coreui and empty migratino

---
+++ app/Http/Controllers/TestTableController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\testTable;
+use Illuminate\Http\Request;
+
+class TestTableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\testTable  $testTable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(testTable $testTable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\testTable  $testTable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(testTable $testTable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\testTable  $testTable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, testTable $testTable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\testTable  $testTable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(testTable $testTable)
+    {
+        //
+    }
+}
